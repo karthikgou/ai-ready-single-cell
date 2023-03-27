957b8a27ba7b406e10c2be5f346e7c03997f2699
Saketh <Saketh Reddy Poduvu>
2023-03-27 14:52:57 -0500

    HOST address

--- ai-single-cell-react/src/components/uploadData.js
+++ ai-single-cell-react/src/components/uploadData.js
@@ -22,7 +22,7 @@ export default function UploadData() {
     const [dirNames, setDirNames] = useState([]);
     const [pwd, setPwd] = useState('/');
     const [selectedItemId, setSelectedItemId] = useState(null);
-    const SERVER_URL = `http://${process.env.HOST_IP}:3001`;
+    const SERVER_URL = `http://${process.env.HOST}:3001`;
     let jwtToken = getCookie('jwtToken');
     const [isNewDirOn, setIsNewDirOn] = useState(false);
 
