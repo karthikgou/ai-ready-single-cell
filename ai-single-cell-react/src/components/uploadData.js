9e0bfae2cd0dc0d8b511355093580bc941e1ab6c
Saketh Reddy Poduvu <48706706+sakred13@users.noreply.github.com>
2023-03-27 00:54:08 -0500

    Update uploadData.js

--- ai-single-cell-react/src/components/uploadData.js
+++ ai-single-cell-react/src/components/uploadData.js
@@ -22,7 +22,7 @@ export default function UploadData() {
     const [dirNames, setDirNames] = useState([]);
     const [pwd, setPwd] = useState('/');
     const [selectedItemId, setSelectedItemId] = useState(null);
-    const SERVER_URL = "node_server";
+    const SERVER_URL = "node_servererr";
     let jwtToken = getCookie('jwtToken');
     const [isNewDirOn, setIsNewDirOn] = useState(false);
 
