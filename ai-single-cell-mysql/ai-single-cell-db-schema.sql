e74c2e8de4665756b516fd421122556394e66535
karthikgou <42573704+karthikgou@users.noreply.github.com>
2023-04-06 16:30:46 -0500

    Updating database name

--- ai-single-cell-mysql/ai-single-cell-db-schema.sql
+++ ai-single-cell-mysql/ai-single-cell-db-schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `directus` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `directus`;
+CREATE DATABASE  IF NOT EXISTS `aisinglecell` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `aisinglecell`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: aisinglecell
